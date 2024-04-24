aedcf4dafd124c725503b7be5bb8011adc8d0244
albert.liu435 <albert.liu435@gmail.com>
2024-04-24 22:34:13 +0800

    day04_01 03-父组件向子组件传值

--- day04_01/03-父组件向子组件传值/App.vue
+++ day04_01/03-父组件向子组件传值/App.vue
@@ -1,29 +1,23 @@
 <template>
-  <div class="App"
-       style="border: 3px dashed #000; margin: 10px;">
-    我是App组件
+  <div class="app" style="border: 3px solid #000; margin: 10px">
+    我是APP组件
     <!-- 1.给组件标签，添加属性方式 赋值 -->
     <Son :title="myTitle"></Son>
   </div>
 </template>
 
 <script>
-import Son from './components/Son.vue';
-
-
-
+import Son from './components/Son.vue'
 export default {
-  name: "App",
-  data () {
+  name: 'App',
+  data() {
     return {
-      myTitle: "黑马程序员"
+      myTitle: '学前端，就来黑马程序员',
     }
   },
   components: {
-
-    Son
-
-  }
+    Son,
+  },
 }
 </script>
 
---
+++ src/components/Son.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="border: 3px solid #000; margin: 10px;">
+    <!-- 3.直接使用props的值 -->
+    我是Son组件 {{title}}
+  </div>
+</template>
+
+<script >
+export default {
+  name: 'Son-Child',
+  // 2.通过props来接受
+  props: ['title']
+}
+</script>
+
+<style scoped>
+.base_count {
+  margin: 20px;
+}
+</style>
