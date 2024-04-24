8c764cc6b47421604b80028570befa66114c57e0
albert.liu435 <albert.liu435@gmail.com>
2024-04-24 22:38:35 +0800

    day04_01 04-子组件想父组件传值

--- day04_01/04-子组件想父组件传值/App.vue
+++ day04_01/04-子组件想父组件传值/App.vue
@@ -1,22 +1,16 @@
 <template>
-  <div class="App"
-       style="border: 3px dashed #000; margin: 10px;">
-    我是App组件
-
+  <div class="app" style="border: 3px solid #000; margin: 10px">
+    我是APP组件
     <!-- 2.父组件对子组件的消息进行监听 -->
-    <Son :title="myTitle"
-         @changTitle="handleChange"></Son>
+    <Son :title="myTitle" @changTitle="handleChange"></Son>
   </div>
 </template>
 
 <script>
-import Son from './components/Son.vue';
-
-
-
+import Son from './components/Son.vue'
 export default {
   name: 'App',
-  data () {
+  data() {
     return {
       myTitle: '学前端，就来黑马程序员',
     }
@@ -26,7 +20,7 @@ export default {
   },
   methods: {
     // 3.提供处理函数，提供逻辑
-    handleChange (newTitle) {
+    handleChange(newTitle) {
       this.myTitle = newTitle
     },
   },
--- src/components/Son.vue
+++ src/components/Son.vue
@@ -1,15 +1,22 @@
 <template>
   <div style="border: 3px solid #000; margin: 10px;">
-    <!-- 3.直接使用props的值 -->
+
     我是Son组件 {{title}}
+    <button @click="changfn">修改title</button>
   </div>
 </template>
 
 <script >
 export default {
   name: 'Son-Child',
-  // 2.通过props来接受
-  props: ['title']
+
+  props: ['title'],
+  methods: {
+    changfn () {
+      // 通过this.$emit() 向父组件发送通知
+      this.$emit('changTitle', '传智教育')
+    },
+  },
 }
 </script>
 
