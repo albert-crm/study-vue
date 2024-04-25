d48fcad8fc91314d82b232a882d41c1a978eac86
albert.liu435 <albert.liu435@gmail.com>
2024-04-25 22:50:08 +0800

    day04_01 15

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <div class="app">
-    <BaseSelect v-model="selectId"></BaseSelect>
+    <div v-if="isShowEdit">
+      <input type="text"
+             v-model="editValue"
+             ref="inp" />
+      <button>确认</button>
+    </div>
+    <div v-else>
+      <span>{{ title }}</span>
+      <button @click="editFn">编辑</button>
+    </div>
   </div>
 </template>
 
 <script>
-import BaseSelect from './components/BaseSelect.vue'
 export default {
   data () {
     return {
-      selectId: '102',
+      title: '大标题',
+      isShowEdit: false,
+      editValue: '',
     }
   },
-  components: {
-    BaseSelect,
+  methods: {
+    editFn () {
+      // 1.显示文本框
+      this.isShowEdit = true
+      // 2.让文本框聚焦 （会等dom更新完之后 立马执行nextTick中的回调函数）
+      // this.$nextTick(() => {
+      //   console.log(this.$refs.inp)
+      //   this.$refs.inp.focus()
+      // })
+
+      setTimeout(() => {
+        this.$refs.inp.focus()
+      }, 0)
+    },
   },
 }
 </script>
