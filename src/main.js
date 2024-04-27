92f5cfb48f1c15c429a8f76cd029a30abf084f70
albert.liu435 <albert.liu435@gmail.com>
2024-04-27 10:16:18 +0800

    day05_01 自定义指令

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.directive("focus", {
+  inserted(el) {
+    console.log(el);
+    el.focus();
+  },
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
