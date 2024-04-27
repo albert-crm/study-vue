68b3fc2cd0e85b7e96deaaa2d4d224396cb831f3
albert.liu435 <albert.liu435@gmail.com>
2024-04-27 12:14:20 +0800

    day05_01 03-v-loading指令封装

--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-Vue.directive("focus", {
-  inserted(el) {
-    console.log(el);
-    el.focus();
-  },
-});
+// Vue.directive("focus", {
+//   inserted(el) {
+//     console.log(el);
+//     el.focus();
+//   },
+// });
+
+// Vue.directive("focus", {
+//   inserted(el) {
+//     console.log(el);
+//     el.focus();
+//   },
+// });
 
 new Vue({
   render: (h) => h(App),
